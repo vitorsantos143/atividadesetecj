9396a9da77838b6ad0a0f02522730c7c9df86eca
Vitor Santos <vitor.santos143@aluno.cps.sp.gov.br>
2026-04-08 17:03:42 -0300

    commit att2

---
+++ att2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace att2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            whatts wtt = new whatts();
+            wtt.pedirNome();
+            wtt.exibirNome();
+        }
+    }
+}
---
+++ att2/whatts.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace att2
+{
+    internal class whatts
+    {
+        string nome;
+        public void pedirNome()
+                   
+        {
+            Console.WriteLine("Digite seu nome");
+            nome = Console.ReadLine();
+        }
+        public void exibirNome()
+        {
+            Console.WriteLine(""+nome);
+        }
+    }
+}
